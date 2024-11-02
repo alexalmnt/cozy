49df113818385e2d10727775b56e81bf22f305c6
EC2 Default User <ec2-user@ip-172-31-16-135.ec2.internal>
2024-11-02 22:56:52 +0000

    new changes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,19 @@
+// Provider block
+provider "aws" {
+  region  = "us-east-1"
+  profile = "Monte"
+}
+// terraform state file stored on S3 bucket
+terraform {
+  backend "s3" {
+    bucket  = "terraform-backend1"
+    key     = "terraform-backend1/backend/backend.tf"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+// Creating Basic EC2 
 resource "aws_instance" "instance1" {
-    ami = "ami-06b21ccaeff8cd686"
-    instance_type = var.instance_type
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = var.instance_type
 }
